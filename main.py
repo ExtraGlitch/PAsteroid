a2ba55d007c748840c9cf497bde48374d537d6d4
ExtraGlitch <florent-melin@outlook.com>
2025-10-03 23:03:39 +0200

    Rate Limit weapon

--- main.py
+++ main.py
@@ -8,26 +8,36 @@ from asteroidfield import *
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    dt = 0
+    #-------------------------------------------------------------------------
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    #-------------------------------------------------------------------------
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+    #-------------------------------------------------------------------------
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
-
+    #-------------------------------------------------------------------------
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    #-------------------------------------------------------------------------
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for object in asteroids:
+            if player.collision(object):
+                print("Game over!")
+                return
         updatable.update(dt)
+        
         pygame.Surface.fill(screen, (0, 0, 0))
         for object in drawable:
             object.draw(screen)
